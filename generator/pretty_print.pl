236597cb0e55fa1ec66c206495c8b16926ed90ca
thron7 <thron7@users.sourceforge.net>
2012-01-30 19:08:52 +0100

    changes

--- generator/pretty_print.pl
+++ generator/pretty_print.pl
@@ -12,17 +12,30 @@ pairs_get_joined(K, [_|J], V) :-
     pairs_get_joined(K, J, V).
     
 
-pretty_open(node_block, '{').
-pretty_close(node_block, '}').
-pretty_open(node_text, '').
+% Attr being a pairs array
+attrib_get(K, Attr, V) :-
+    pairs_get(K, Attr, [V|_]).
+
+pretty_open(node_block, _, '{').
+pretty_close(node_block, _, '}').
+pretty_open(node_text, Attr, V) :- attrib_get(value, Attr, V).
+pretty_close(node_text, _, '').
+
+% :- X=[[node_text, [value-'FooManChu'], []]], [[N,A,_]|R] = X,pretty_open(N,A,V).
+
+% some test trees
+forest(f1, [[node_text, [value-'FooManChu'], []]]).
+forest(f2, [[node_block, [], [[node_text, [value-'FooManChu'], []]]]]).
 
 
-pretty_print([[Node, Attribs, Children]|N], S) :-
-    pretty_open(Node, SOpen),
-    pretty_close(Node, SClose),
+% pretty_print(+List_of_trees, -List_of_strings)
+pretty_print([], ['']) :- !.
+pretty_print([[Node, Attribs, Children]|N], L) :-
+    pretty_open(Node, Attribs, SOpen),
+    pretty_close(Node, Attribs, SClose),
     pretty_print(Children, SChildren),
     pretty_print(N, SN),
-    append([[SOpen], SChildren, [SClose], SN], S).
+    append([[SOpen], SChildren, [SClose], SN], L).
 
 
 % vim: ts=4 sw=4
